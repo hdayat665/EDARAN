dbf59658617d0ac557123f4368bc82ab58f24f1f
ahmad-muttaqin <ahmad.muttaqin.khairuddin@gmail.com>
2022-08-31 19:28:10 +0800

    update on ui
    update

--- resources/views/pages/auth/package.blade.php
+++ resources/views/pages/auth/package.blade.php
@@ -2,85 +2,88 @@
 
 @section('content')
 
-<div class="shadow p-3 mb-5 bg-body rounded" style="display: flex; justify-content: center;">
-    <div class="card border-0 col-sm-3" style="width:600px; min-width:300px">
-        <img src="{{env('ASSETS_URL')}}/img/logo/orbit-logo-5.png" class="img-fluid" style="margin:9vh"  alt="Orbit Logo">
-        <h1 class="text-center text-primary">Choose package : </h1>
-        <div class="card border-0 shadow-sm p-3 mb-5" style="margin:2vh; padding:2vh">
-            <i class="fas fa-box fa-7x text-center "></i>
-            <h2 class="text-center">BASIC</h2>
-            <h3 class="text-center text-gray">Free</h3>
-            <div class="card border-0" style="padding:2vh">
-                <p class="text-center text-green">
-                    <i class="fa-solid fa-square-check"></i>
-                    Maximum User Count : 100
-                </p>
-                <p class="text-center text-green">
-                    <i class="fa-solid fa-square-check"></i>
-                    Test check feature
-                </p>
-                <p class="text-center text-danger">
-                    <i class="fa-solid fa-ban"></i>
-                    Test check feature 2
-                </p>
-            </div>
-            <div class="mb-15px">
-                <a href="/registerTenant/basic"><button type="submit" class="btn btn-primary d-block h-45px w-100 btn-lg fs-14px">Basic Package</button></a>
-            </div>
-        </div>
-        <div class="card border-0 shadow-sm p-3 mb-5" style="margin:2vh; padding:2vh">
-            <i class="fas fa-gem fa-7x text-center "></i>
-            <h2 class="text-center">GOLD</h2>
-            <h4 class="text-center text-gray">RM 199.99 / YEARLY</h4>
-            <div class="card border-0" style="padding:2vh">
-                <p class="text-center text-green">
-                    <i class="fa-solid fa-square-check"></i>
-                    Maximum User Count : Unlimited
-                </p>
-                <p class="text-center text-green">
-                    <i class="fa-solid fa-square-check"></i>
-                    Test check feature
-                </p>
-                <p class="text-center text-green">
-                    <i class="fa-solid fa-square-check"></i>
-                    Test check feature 2
-                </p>
-            </div>
-            <div class="mb-15px">
-                <a href="/registerTenant/gold"><button type="submit" class="btn btn-primary d-block h-45px w-100 btn-lg fs-14px">Gold Package</button></a>
-            </div>
-        </div>
-        <div class="card border-0 shadow-sm p-3 mb-5" style="margin:2vh; padding:2vh">
-            <i class="fas fa-box fa-7x text-center "></i>
-            <h2 class="text-center">PLATINUM</h2>
-            <h4 class="text-center text-gray">RM 299.99 / YEARLY</h4>
-            <div class="card border-0" style="padding:2vh">
-                <p class="text-center">
-                    RM1 PERDAY <br>
-                    RM7 PERWEEK <br>
-                    RM30 PERMONTH<br>
-                    299.99 PERYEAR
-
-                    <p class="text-center text-green">
-                        <i class="fa-solid fa-square-check"></i>
-                        Maximum User Count : Unlimited
-                    </p>
-                    <p class="text-center text-green">
-                        <i class="fa-solid fa-square-check"></i>
-                        Test check feature
-                    </p>
-                    <p class="text-center text-green">
-                        <i class="fa-solid fa-square-check"></i>
-                        Test check feature 2
-                    </p>
-                </p>
-            </div>
-            <div class="mb-15px">
-                <a href="/registerTenant/premium"><button type="submit" class="btn btn-primary d-block h-45px w-100 btn-lg fs-14px">Platinum Package</button></a>
-            </div>
-        </div>
-
-    </div>
+<div class="text-center mt-40px">
+    <img src="{{env('ASSETS_URL')}}/img/logo/orbit-logo-5.png" class="img-fluid" style="margin:3h"  alt="Orbit Logo">
 </div>
+
+<div class="row row-cols-1 row-cols-md-3 g-4 mt-50px">
+  <div class="col">
+    <div class="card h-100">
+                 <div class="card-body text-center">
+                            <i class="fas fa-box fa-7x text-center "></i>
+                            <h2 class="text-center mt-30px">BASIC</h2>
+                            <h3 class="text-center text-gray">Free</h3>
+                                <div class="card border-0" style="padding:2vh">
+                                    <p class="text-center text-green">
+                                        <i class="fa-solid fa-square-check"></i>    
+                                        Maximum User Count : 100</p>
+                                    <p class="text-center text-green">
+                                        <i class="fa-solid fa-square-check"></i>    
+                                    Test check feature</p>
+                                    <p class="text-center text-danger">
+                                        <i class="fa-solid fa-ban"></i>    
+                                        Test check feature 2</p>
+                                </div>
+              </div>
+                    <div class="mb-15px">
+                    <a href="/registerTenant/basic"><button type="submit" class="btn btn-primary d-block h-45px w-100 btn-lg fs-14px">Basic Package</button></a>
+                    </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card h-100">
+                 <div class="card-body text-center">
+                 <i class="fas fa-gem fa-7x text-center "></i>
+                <h2 class="text-center mt-30px">GOLD</h2>
+                <h4 class="text-center text-gray">RM 199.99 / YEARLY</h4>
+				<div class="card border-0" style="padding:2vh">
+                    <p class="text-center text-green">
+                        <i class="fa-solid fa-square-check"></i>    
+                        Maximum User Count : Unlimited</p>
+                    <p class="text-center text-green">
+                        <i class="fa-solid fa-square-check"></i>    
+                       Test check feature</p>
+					   <p class="text-center text-green">
+					   <i class="fa-solid fa-square-check"></i>     
+						Test check feature 2</p>
+                                </div>
+              </div>
+                    <div class="mb-15px">
+                    <a href="/registerTenant/gold"><button type="submit" class="btn btn-primary d-block h-45px w-100 btn-lg fs-14px">Gold Package</button></a>
+                    </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+                 <div class="card-body text-center">
+                 <i class="fas fa-gift fa-7x text-center "></i>
+                <h2 class="text-center mt-30px">PLATINUM</h2>
+                <h4 class="text-center text-gray">RM 299.99 / YEARLY</h4>
+                <div class="card border-0" style="padding:2vh">
+                    <p class="text-center">
+                        RM1 PERDAY <br>
+						RM7 PERWEEK <br>
+						RM30 PERMONTH<br>
+						299.99 PERYEAR
+                    
+						<p class="text-center text-green">
+                        <i class="fa-solid fa-square-check"></i>    
+                        Maximum User Count : Unlimited</p>
+                    <p class="text-center text-green">
+                        <i class="fa-solid fa-square-check"></i>    
+                       Test check feature</p>
+					   <p class="text-center text-green">
+					   <i class="fa-solid fa-square-check"></i>     
+						Test check feature 2</p>
+                                </div>
+              </div>
+                    <div class="mb-15px">
+                    <a href="/registerTenant/premium"><button type="submit" class="btn btn-primary d-block h-45px w-100 btn-lg fs-14px">Platinum Package</button></a>
+                    </div>
+        </div>
+    </div>
+    
+  </div>
 
 @endsection
