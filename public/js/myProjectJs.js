a5107e285e4c2b694123c723adbacb1bffb6287a
Hidayat <hidayat@thelorry.com>
2022-09-19 08:14:46 +0800

    update

--- public/js/myProjectJs.js
+++ public/js/myProjectJs.js
@@ -5,4 +5,50 @@ $(document).ready(function() {
         lengthMenu: [5, 10, 15],
     });
 
+    $("#myProjectPendingTable").DataTable({
+        responsive: true,
+        lengthMenu: [5, 10, 15],
+    });
+
+    $("#myProjectRejectTable").DataTable({
+        responsive: true,
+        lengthMenu: [5, 10, 15],
+    });
+
+    $(document).on("click", "#cancelProject", function() {
+        id = $(this).data('id');
+        requirejs(['sweetAlert2'], function(swal) {
+            swal({
+                title: "Are you sure!",
+                type: "error",
+                confirmButtonClass: "btn-danger",
+                confirmButtonText: "Yes!",
+                showCancelButton: true,
+            }).then(function() {
+                $.ajax({
+                    type: "POST",
+                    url: "/cancelProjectMember/" + id,
+                    dataType: "json",
+                    async: false,
+                    processData: false,
+                    contentType: false,
+                }).done(function(data) {
+                    swal({
+                        title: data.title,
+                        text: data.msg,
+                        type: data.type,
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'OK'
+                    }).then(function() {
+                        if (data.type == 'error') {
+
+                        } else {
+                            location.reload();
+                        }
+                    });
+                });
+            });
+        });
+    });
+
 });
