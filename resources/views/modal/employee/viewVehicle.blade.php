51620b052282c501a991c1102c7ffabf637de616
phantom393 <azrul393@gmail.com>
2022-10-19 15:37:02 +0800

    validation and fix bug on vehicle and change password

--- resources/views/modal/employee/viewVehicle.blade.php
+++ resources/views/modal/employee/viewVehicle.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-            <h5 class="modal-title" id="add-parent">New Vehicle</h5>
+            <h5 class="modal-title" id="add-parent">New Vehi</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
