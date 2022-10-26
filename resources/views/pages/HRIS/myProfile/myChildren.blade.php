0f0b1f77c45d4ff59b93f13ddbc2720ea92dac69
anisfarahana <farahzed21@gmail.com>
2022-10-26 13:01:46 +0800

    update

--- resources/views/pages/HRIS/myProfile/myChildren.blade.php
+++ resources/views/pages/HRIS/myProfile/myChildren.blade.php
@@ -20,7 +20,7 @@
             <tr>
                 <td width="1%" class="fw-bold text-dark">1</td>
                 <td>
-                    <a href="#" data-bs-toggle="dropdown" class="btn btn-primary dropdown-toggle"><i class="fa fa-cogs"></i> Actions <i class="fa fa-caret-down"></i></a>
+                    <a href="#" data-bs-toggle="dropdown" class="btn btn-primary dropdown-toggle btn-sm"><i class="fa fa-cogs"></i> Actions <i class="fa fa-caret-down"></i></a>
                     <div class="dropdown-menu">
                         <a href="javascript:;" data-bs-toggle="modal" id="childModalEdit{{$children->id}}" data-id="{{$children->id}}" data-type="edit" class="dropdown-item">Edit</a>
                         <div class="dropdown-divider"></div>
