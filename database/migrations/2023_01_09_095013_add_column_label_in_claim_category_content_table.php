1215276eda6fc6503723189b25cffa5cbe8b7a8a
Hidayat <hidayat@DESKTOP-4LADPD7>
2023-01-09 10:17:04 +0800

    update

---
+++ database/migrations/2023_01_09_095013_add_column_label_in_claim_category_content_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('claim_category_content', function (Blueprint $table) {
+            $table->string('label', 255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('claim_category_content', function (Blueprint $table) {
+            $table->dropColumn('label');
+        });
+    }
+};
