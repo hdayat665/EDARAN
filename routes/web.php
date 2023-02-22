61a26415daecb77ef58e41525b8710e30821ff49
hidayat <hidayat@DESKTOP-4LADPD7>
2023-02-22 10:29:25 +0800

    update ca approval

--- routes/web.php
+++ routes/web.php
@@ -123,6 +123,8 @@ Route::group(['middleware' => ['web']], function () {
 
             Route::post('/createChequeNumberCa/{id}', 'createChequeNumberCa');
             Route::post('/createPvNumberCa/{id}', 'createPvNumberCa');
+            Route::post('/createClearCa/{id}', 'createClearCa');
+
 
 
 
