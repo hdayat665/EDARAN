abe2fb7cdaaed96e2a976383fee465080ed09fd2
naim <60913818+phantom393@users.noreply.github.com>
2023-05-22 21:23:59 +0800

    update bug appeal

--- public/js/myTimesheetJs.js
+++ public/js/myTimesheetJs.js
@@ -1178,8 +1178,9 @@ $(document).ready(function () {
                             var hours = Math.floor(totalHour / 3600);
                             var minutes = Math.floor((totalHour % 3600) / 60);
                             totalHours1 = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
+                            var [hours1, minutes] = totalHours1.split(":").map(Number);
 
-
+                            // console.log(hours1)
                             // Exit the loop once the total hours for the current date are calculated
                             break;
                         }
@@ -1238,96 +1239,51 @@ $(document).ready(function () {
                             top: '-35px',
                             'z-index': '999',
                         });
-                    }if (
-                        (current.getMonth() === currentDate.getMonth()) &&
-                        (current.getDate() === oneDayBefore.getDate() || current.getDate() === twoDayBefore.getDate()) &&
-                        !(current.getDay() === 6 || current.getDay() === 0) && hasLog && totalHours >= 9
-                      ) {
-                        $(info.el).css('background-color', '#80FF80');
-                      } else if(
-                        (current.getMonth() === currentDate.getMonth()) &&
-                        (current.getDate() === oneDayBefore.getDate() || current.getDate() === twoDayBefore.getDate()) &&
-                        !(current.getDay() === 6 || current.getDay() === 0)  && (hasLog || !hasLog || totalHours < 9)
-                      ) {
-                        $(info.el).css('background-color', '#FF8080');
-                      }
-                      else if((current < duahari) &&  !appliedDates.includes(datedefaultformat) && !hasLog && !(current.getDay() === 6 || current.getDay() === 0)) {
-                        $(info.el).css('background-color', '#FF8080');
 
+                      } 
+
+                      //xde log appeal, log kecik dari 9,exclude weekend
+                       if((current < duahari) && !appliedDates.includes(datedefaultformat) && (hasLog && hours1 < 9 || !hasLog)   && !(current.getDay() === 6 || current.getDay() === 0)) {
+                        $(info.el).css('background-color', '#FF8080');
                         $(info.el).append('&nbsp;').append(appealaddb);
                         $(appealaddb).css({
                             position: 'relative',
                             top: '-35px',
-                            'z-index': '999',
+                            'z-index': '999',   
                             });
-                    } else if ((current < duahari) &&  appliedDates.includes(datedefaultformat) && !hasLog && !(current.getDay() === 6 || current.getDay() === 0)) {
-                        $(info.el).css('background-color', '#FF8080');
-                        
 
+                        }else if((current < duahari) && appliedDates.includes(datedefaultformat) && (hasLog && hours1 < 9 || !hasLog)   && !(current.getDay() === 6 || current.getDay() === 0)) {
+
+                            $(info.el).css('background-color', '#FF8080');
                             $(info.el).append('&nbsp;').append(viewappealb);
                             $(viewappealb).css({
                                 position: 'relative',
                                 top: '-35px',
-                                'z-index': '999',
-                              });
-
-                }else if (hasLog && totalHours >= 9) {
-                        $(info.el).css('background-color', '#80FF80');
-                    } else if(hasLog && (current.getDay() === 6 || current.getDay() === 0)) {
-                        $(info.el).css('background-color', '#B3CCFF');
-                    }else if (hasLog && totalHours <= 9) {
-                        if  ((current.getDate() === oneDayBefore.getDate() || current.getDate() === twoDayBefore.getDate()) && hasLog && !appliedDates.includes(datedefaultformat)) { //kat sini
-                            $(info.el).css('background-color', '#FF8080');
-                            $(info.el).append('&nbsp;').append(appealaddb);
-                            $(appealaddb).css({
-                                position: 'relative',
-                                top: '-35px',
-                                'z-index': '999',
+                                'z-index': '999',   
                                 });
-                            // $(info.el).append('&nbsp;').append(viewappealb);
-                            //     $(viewappealb).css({
-                            //         position: 'relative',
-                            //         top: '-35px',
-                            //         'z-index': '999',
-                            //       });
-
-                            } else if ((current.getDate() === oneDayBefore.getDate() || current.getDate() === twoDayBefore.getDate()) && hasLog && appliedDates.includes(datedefaultformat) ){
-                                $(info.el).css('background-color', '#FF8080');
-                                $(info.el).append('&nbsp;').append(viewappealb);
-                                    $(viewappealb).css({
-                                        position: 'relative',
-                                        top: '-35px',
-                                        'z-index': '999',
-                                      });
-                            }else if (appliedDates.includes(datedefaultformat) && current.getFullYear() === info.date.getFullYear() && current.getMonth() === info.date.getMonth()) {
-
-
-                                $(info.el).css('background-color', '#FF8080');
-
-                                $(info.el).append('&nbsp;').append(viewappealb);
-                                $(viewappealb).css({
-                                    position: 'relative',
-                                    top: '-35px',
-                                    'z-index': '999',
-                                  });
-
-                            }else {
-                                if (current.getDate() === currentdate.getDate()) {
-                                    $(info.el).css('background-color', '#FF8080');
-                                }
-                                else {
-                            $(info.el).css('background-color', '#FF8080');
-                            $(info.el).append('&nbsp;').append(appealaddb);
-                            $(appealaddb).css({
+                        
+                        
+                         } else if((current < duahari) && hasLog && hours1 >= 9) {
+                            $(info.el).css('background-color', '#80ff80');
+                         } else if((current < duahari) && appliedDates.includes(datedefaultformat) && hours1 > 9) {
+                            $(info.el).css('background-color', '#80ff80');
+                            $(info.el).append('&nbsp;').append(viewappealb);
+                            $(viewappealb).css({
                                 position: 'relative',
                                 top: '-35px',
-                                'z-index': '999',
-                              });
-                                }
-    
-
-                        }
-                    } else if (info.date.getDay() === 0 &&  !isSameDate) {
+                                'z-index': '999',   
+                                });
+                         }
+                         else if((current < duahari) && appliedDates.includes(datedefaultformat) && hours1 < 9) {
+                            $(info.el).css('background-color', '#FF8080');
+                            $(info.el).append('&nbsp;').append(viewappealb);
+                            $(viewappealb).css({
+                                position: 'relative',
+                                top: '-35px',
+                                'z-index': '999',   
+                                });
+                         }
+                         else if (info.date.getDay() === 0 &&  !isSameDate) {
                         $(info.el).css('background-color', '#B3CCFF');
                     } else if (info.date.getDay() === 6 && !isSameDate) {
                         $(info.el).css('background-color', '#B3CCFF');
