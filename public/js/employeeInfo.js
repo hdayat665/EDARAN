a14f5b17638f29544670b6edf4f2a1cf740b28ce
Chieynaf <hanieschieynaf@gmail.com>
2023-07-24 23:06:50 +0800

    update
    bugs and tasks

--- public/js/employeeInfo.js
+++ public/js/employeeInfo.js
@@ -2,19 +2,20 @@ $(document).ready(function () {
 
     $(document).ready(function() {
         $(".wan").hide();
-
-        $(".dropdown-toggle").on("click", function() {
+    
+        $(document).on("click", ".dropdown-toggle", function() {
             var dropdownMenu = $(this).closest(".btn-group").find(".wan");
             $(".wan").not(dropdownMenu).hide();
             dropdownMenu.toggle();
         });
-
+    
         $(document).on("click", function(e) {
             if (!$(".btn-group").is(e.target) && $(".btn-group").has(e.target).length === 0) {
                 $(".wan").hide();
             }
         });
     });
+    
 
     $("#tableemployeeinfo").DataTable({
         responsive: false,
