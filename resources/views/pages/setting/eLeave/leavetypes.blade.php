8906fd018bed553508817fbab3c832ef38298e07
Chieynaf <hanieschieynaf@gmail.com>
2023-08-16 11:04:54 +0800

    UPDATE
    SETTING BACK BUTTON

--- resources/views/pages/setting/eLeave/leavetypes.blade.php
+++ resources/views/pages/setting/eLeave/leavetypes.blade.php
@@ -66,6 +66,12 @@
 						</table>
 					</div>
 				</div>
+				<div class="row p-2">
+                <div class="col align-self-start">
+                    <a href="/setting" class="btn btn-light" style="color: black;" type="submit"><i class="fa fa-arrow-left"></i> Back</a>
+                </div>
+            </div>
+
 			</div>
 		</div>
         @include('modal.eleave.leavetypes.leaveTypesModelStore')
