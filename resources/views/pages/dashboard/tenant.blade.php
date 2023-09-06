bb2380194dd5dc9edf3a1cd8ca2300598740d841
Aidil-Maula <m.aidilmaula@gmail.com>
2023-09-06 10:18:14 +0800

    refix task - 45826

--- resources/views/pages/dashboard/tenant.blade.php
+++ resources/views/pages/dashboard/tenant.blade.php
@@ -103,7 +103,7 @@
 								<td>{{ $new['title'] }}</td>
 								<td>{{ $new['content'] }}</td>
 								<td><a href="{{ $new['sourceURL'] }}" target="_blank">{{ $new['sourceURL'] }}</a></td>
-                                <td><a href="{{ route('view', ['filename' => $new->file]) }}" target="blank_">{{$new->file}}</a></td>
+                                <td><a href="{{ route('view', ['filename' => $new->file]) }}" target="_blank">{{$new->file}}</a></td>
 							</tr>
 						@endforeach
 						@endif
