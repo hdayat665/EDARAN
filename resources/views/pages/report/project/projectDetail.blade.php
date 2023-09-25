c1d4a8930ff1cab67713c4e0385d601fd2679c3b
Chieynaf <hanieschieynaf@gmail.com>
2023-09-25 09:31:23 +0800

    Update project report
    46384 - [Web Pilot Test] Reporting - Download Project Listing Report - Display all column as in table list
    46382 - [Web Pilot Test] Reporting - Export Project Listing Report - Display all column as in table list

--- resources/views/pages/report/project/projectDetail.blade.php
+++ resources/views/pages/report/project/projectDetail.blade.php
@@ -10,15 +10,15 @@
             </div>
             <form>
                 <div class="row mb-15px">
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                         <label class="form-label col-form-label col-md-4">Customer Name*</label>
                         <input type="customer name" readonly class="form-control mb-5px" value="{{$detailProject->customer_name}}" />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label col-md-4">Project Code*</label>
                         <input type="Project Code" readonly class="form-control mb-5px" value="{{$detailProject->project_code}}" />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label col-md-4">Project Name*</label>
                         <input type="Project name" readonly class="form-control mb-5px" value="{{$detailProject->project_name}}" />
                     </div>
@@ -30,63 +30,63 @@
                     </div>
                 </div>
                 <div class="row mb-15px">
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                         <label class="form-label col-form-label col-md-4">Contract Value*</label>
                         <input type="contract-value" readonly class="form-control mb-5px" value="{{$detailProject->contract_value}}" />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                         <label class="form-label col-form-label col-md-4">Contract Type</label>
                         <input type="contract-type" readonly class="form-control mb-5px" value="{{$detailProject->contract_type}}" />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label col-md-4">Financial Year*</label>
                         <input type="financial-year" readonly class="form-control mb-5px" value="{{$detailProject->financial_year}}" />
                     </div>
                 </div>
                 <div class="row mb-15px">
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label col-md-4">LOA Date*</label>
                         <input type="LOA-date" readonly class="form-control mb-5px" value="{{$detailProject->LOA_date}}" />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label col-md-4">Contract Start Date*</label>
                         <input type="contract-start-date" readonly class="form-control mb-5px" value="{{$detailProject->contract_start_date}}"  />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label col-md-4">Contract End Date*</label>
                         <input type="contract-end-date" readonly class="form-control mb-5px" value="{{$detailProject->contract_end_date}}" />
                     </div>
                 </div>
                 <div class="row mb-15px">
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                         <label class="form-label col-form-label col-md-4">Account Manager*</label>
                         <input type="account-manager" readonly class="form-control mb-5px" value="{{($detailProject->acc_manager) ? getEmployeeNameById($detailProject->acc_manager)->employeeName ?? '-' : '-'}}" />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                         <label class="form-label col-form-label col-md-4">Project Manager*</label>
                         <input type="project-manager" readonly class="form-control mb-5px" value="{{($detailProject->project_manager) ? getEmployeeNameById($detailProject->project_manager)->employeeName ?? '-' : '-'}}" />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label col-md-4">Warranty Start Date</label>
                         <input type="warranty-start-date" readonly class="form-control mb-5px" value="{{$detailProject->warranty_start_date}}" />
                     </div>
                 </div>
                 <div class="row mb-15px">
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                         <label class="form-label col-form-label">Warranty End Date</label>
                         <input type="warranty-end-date" readonly class="form-control mb-5px" value="{{$detailProject->warranty_end_date}}" />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label ">Bank Guarantee Amount*</label>
                         <input type="bank-gurantee-amount" readonly class="form-control mb-5px" value="{{$detailProject->bank_guarantee_amount}}" />
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label">Bank Guarantee Expiry Date</label>
                         <input type="bank-gurantee-expiry" readonly class="form-control mb-5px" value="{{$detailProject->bank_guarantee_expiry_date}}" />
                     </div>
                 </div>
                 <div class="row mb-15px">
-                    <div class="col-md-4">
+                    <div class="col-md-4 next">
                     <label class="form-label col-form-label col-md-4">Status</label>
                         <input type="status" readonly class="form-control mb-5px" value="{{$detailProject->status}}" />
                     </div>
@@ -123,6 +123,32 @@
                 #projectMemberTable_wrapper .dataTables_length,
                 #projectMemberTable_wrapper .dataTables_filter {
                     display: none;
+                }
+                input[type] {
+                    width: 100%;
+                    padding: 2px 2px;
+                    margin: 2px 2px;
+                    /* box-sizing: border-box; */
+                    border: none;
+                    -webkit-transition: 0.5s;
+                    transition: 0.5s;
+                    outline: none;
+                }
+                textarea {
+                    border: none !important;
+                    padding: 1px 2px;
+                    margin: 2px 0;
+                    outline: none;
+                    width: 100%;
+                }
+                .next {
+                    float: left;
+                    width: 33%; 
+                }
+                .clearfix::after {
+                    content: "";
+                    clear: both;
+                    display: table;
                 }
             }
         </style>
